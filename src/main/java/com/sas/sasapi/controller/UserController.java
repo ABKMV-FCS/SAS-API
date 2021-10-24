b5de07fa9b397860c559c9e890f2e92f9db74f2a
Monish Raaj L <monishraaj05@gmail.com>
2021-10-25 02:48:53 +0530

    reset password route added

--- src/main/java/com/sas/sasapi/controller/UserController.java
+++ src/main/java/com/sas/sasapi/controller/UserController.java
@@ -2,10 +2,16 @@ package com.sas.sasapi.controller;
 
 import com.sas.sasapi.exception.ResourceNotFound;
 import com.sas.sasapi.model.User;
+import com.sas.sasapi.payload.request.PasswordRequest;
+import com.sas.sasapi.payload.response.MessageResponse;
 import com.sas.sasapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +22,10 @@ import java.util.List;
 public class UserController {
 
     private final UserRepository userRepository;
+
+
+    @Autowired
+    PasswordEncoder encoder;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -52,6 +62,21 @@ public class UserController {
         User userObj = userRepository.findByUserId(user.getUserId()).orElseThrow(() -> new ResourceNotFound("Cannot find user in db"));
         userRepository.delete(userObj);
         return new ResponseEntity<>(userObj,HttpStatus.OK);
+
+    }
+
+    @PostMapping("/resetpassword")
+    public  ResponseEntity<MessageResponse> resetPassword(@RequestBody PasswordRequest passwordRequest){
+        try {
+            UserDetails userDetails = (UserDetails) SecurityContextHolder. getContext(). getAuthentication().getPrincipal();
+            String username = userDetails. getUsername();
+            User user = (User) userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+            user.setPassword(encoder.encode(passwordRequest.getPassword()));
+            userRepository.save(user);
+            return ResponseEntity.ok().body(new MessageResponse("password reset successfully!"));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
+        }
 
     }
 }
